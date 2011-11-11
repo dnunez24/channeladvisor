1e9fb809401b0d7e23c6bbd9429f1741614ddc98
Dave Nunez <david.nunez@berrettbrothers.com>
2011-11-11 14:16:07 -0800

    Create Configuration Module
    Create the Configuration module and extend in the ChannelAdvisor base module for easy access to configuration options. The configure method accepts block arguments to set access credentials that are needed by the ChannelAdvisor API.

--- lib/channeladvisor.rb
+++ lib/channeladvisor.rb
@@ -1,6 +1,11 @@
 require 'channeladvisor/version'
+require 'channeladvisor/configuration'
 require 'channeladvisor/connection'
 
 module ChannelAdvisor
+	extend Configuration
 
+	def self.configure(&block)
+		yield self if block_given?
+	end
 end
---
+++ lib/channeladvisor/configuration.rb
@@ -0,0 +1,5 @@
+module ChannelAdvisor
+	module Configuration
+		attr_accessor :account_id, :developer_key, :password
+	end
+end
