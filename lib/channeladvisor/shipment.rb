d1e825705d1a63f9886541e5c4b6d8ee5a2d272e
Dave Nunez <dnunez24@gmail.com>
2012-06-12 23:04:57 -0700

    Fix Spacing on Shipment.rb

--- lib/channeladvisor/shipment.rb
+++ lib/channeladvisor/shipment.rb
@@ -9,14 +9,14 @@ module ChannelAdvisor
     end
 
     def self.submit(shipment_data)
-    	shipments = arrayify(shipment_data).each do |shipment|
-	    	shipment[:type] = shipment[:line_items] ? "Partial" : "Full"
-	    	shipment[:date] ||= DateTime.now
-	    	shipment[:cost] 			= "%.2f" % shipment[:cost].to_f
-	    	shipment[:tax] 				= "%.2f" % shipment[:tax].to_f
-	    	shipment[:insurance] 	= "%.2f" % shipment[:insurance].to_f
-	    end
-    	Services::ShippingService.submit_order_shipment_list(shipments)
+      shipments = arrayify(shipment_data).each do |shipment|
+        shipment[:type] = shipment[:line_items] ? "Partial" : "Full"
+        shipment[:date] ||= DateTime.now
+        shipment[:cost]       = "%.2f" % shipment[:cost].to_f
+        shipment[:tax]        = "%.2f" % shipment[:tax].to_f
+        shipment[:insurance]  = "%.2f" % shipment[:insurance].to_f
+      end
+      Services::ShippingService.submit_order_shipment_list(shipments)
     end
   end
 end
