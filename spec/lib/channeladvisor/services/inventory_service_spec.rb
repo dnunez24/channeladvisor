511ebb5aca006814ff5c99c3d7c6aaed00f32366
Dave Nunez <dnunez24@gmail.com>
2012-06-14 22:23:27 -0700

    Complete Inventory Service Update Quantity and Price

--- spec/lib/channeladvisor/services/inventory_service_spec.rb
+++ spec/lib/channeladvisor/services/inventory_service_spec.rb
@@ -1,9 +1,30 @@
 require 'spec_helper'
 
 module ChannelAdvisor
-	module Services
-		describe InventoryService do
-			describe ".update_inventory_item_quantity_and_price" do
+  module Services
+    describe InventoryService do
+      describe ".update_inventory_item_quantity_and_price" do
+        use_vcr_cassette "responses/inventory_service/update_inventory_item_quantity_and_price"
+
+        let(:quantity_info) do
+          {
+            :update_type  => "Absolute",
+            :total        => 5000
+          }
+        end
+
+        let(:price_info) do
+          {
+            :cost                       => 2.99,
+            :retail_price               => 11.99,
+            :starting_price             => 5.99,
+            :reserve_price              => 7.99,
+            :take_it_price              => 9.99,
+            :second_chance_offer_price  => 8.99,
+            :store_price                => 9.49
+          }
+        end
+
         before(:each) do
           @last_request, @last_response = nil
 
@@ -13,37 +34,38 @@ module ChannelAdvisor
           end
         end
 
-        context "with only quantity info" do
-        	use_vcr_cassette "responses/inventory_service/update_item_quantity"
+        it "returns a SOAP response" do
+          soap_response = InventoryService.update_inventory_item_quantity_and_price("FAKE001", :quantity_info => quantity_info, :price_info => price_info)
+          soap_response.should be_a Savon::SOAP::Response
+        end # returns a SOAP response
 
-				  it "sends a valid SOAP request with only quantity info" do
-				  	quantity_info = {
-				  		:update_type 	=> "Absolute",
-				  		:total 				=> 5000
-				  	}
-				    InventoryService.update_inventory_item_quantity_and_price("FAKE001", :quantity_info => quantity_info)
-				    @last_request.should match_valid_xml_body_for :update_item_quantity
-				  end
+        context "with only quantity info" do
+          use_vcr_cassette "responses/inventory_service/update_inventory_item_quantity_and_price/quantity_only"
+
+          it "sends a valid SOAP request with only quantity info" do
+            InventoryService.update_inventory_item_quantity_and_price("FAKE001", :quantity_info => quantity_info)
+            @last_request.should match_valid_xml_body_for :update_item_quantity
+          end
         end # with only quantity data
 
         context "with only price info" do
-        	use_vcr_cassette "responses/inventory_service/update_item_price"
+          use_vcr_cassette "responses/inventory_service/update_inventory_item_quantity_and_price/price_only"
 
-				  it "sends a valid SOAP request with only price info" do
-				  	price_info = {
-				  		:cost 											=> 2.99,
-				  		:retail_price 							=> 11.99,
-				  		:starting_price 						=> 5.99,
-				  		:reserve_price 							=> 7.99,
-				  		:take_it_price 							=> 9.99,
-				  		:second_chance_offer_price 	=> 8.99,
-				  		:store_price 								=> 9.49
-				  	}
-				    InventoryService.update_inventory_item_quantity_and_price("FAKE001", :price_info => price_info)
-				    @last_request.should match_valid_xml_body_for :update_item_price
-				  end
+          it "sends a valid SOAP request with only price info" do
+            InventoryService.update_inventory_item_quantity_and_price("FAKE001", :price_info => price_info)
+            @last_request.should match_valid_xml_body_for :update_item_price
+          end
         end # with only price data
-			end # .update_inventory_item_quantity_and_price
-		end # InventoryService
-	end # Services
+
+        context "with both quantity and price info" do
+          use_vcr_cassette "responses/inventory_service/update_inventory_item_quantity_and_price/quantity_and_price"
+
+          it "sends a valid SOAP request with both quantity and price info" do
+            InventoryService.update_inventory_item_quantity_and_price("FAKE001", :quantity_info => quantity_info, :price_info => price_info)
+            @last_request.should match_valid_xml_body_for :update_item_quantity_and_price
+          end
+        end # with only price data
+      end # .update_inventory_item_quantity_and_price
+    end # InventoryService
+  end # Services
 end # ChannelAdvisor
