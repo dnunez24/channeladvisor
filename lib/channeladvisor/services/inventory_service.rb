4140a8a23d1d513549b44ccacd7a1de765cd5bf3
Dave Nunez <dnunez24@gmail.com>
2012-06-14 23:20:41 -0700

    Add InventoryService.update_inventory_quantity_and_price_list

--- lib/channeladvisor/services/inventory_service.rb
+++ lib/channeladvisor/services/inventory_service.rb
@@ -37,8 +37,45 @@ module ChannelAdvisor
             soap.header = soap_header
             soap.body = soap_body
           end
-        end
-      end
-    end
-  end
-end
+        end # update_inventory_item_quantity_and_price
+
+        def update_inventory_item_quantity_and_price_list(quantity_and_price_list)
+          soap_body = {
+            "ins0:accountID" => creds(:account_id),
+            "ins0:itemQuantityAndPriceList" => {
+              "ins0:InventoryItemQuantityAndPrice" => {
+                "ins0:Sku" => quantity_and_price_list.first[:sku]
+              }
+            }
+          }
+
+          item_quantity_and_price = soap_body["ins0:itemQuantityAndPriceList"]["ins0:InventoryItemQuantityAndPrice"]
+
+          if quantity_info = quantity_and_price_list.first[:quantity_info]
+            item_quantity_and_price["ins0:QuantityInfo"] = {
+              "ins0:UpdateType" => quantity_info[:update_type],
+              "ins0:Total"      => quantity_info[:total]
+            }
+          end
+
+          if price_info = quantity_and_price_list.first[:price_info]
+            item_quantity_and_price["ins0:PriceInfo"] = {
+              "ins0:Cost"                   => price_info[:cost],
+              "ins0:RetailPrice"            => price_info[:retail_price],
+              "ins0:StartingPrice"          => price_info[:starting_price],
+              "ins0:ReservePrice"           => price_info[:reserve_price],
+              "ins0:TakeItPrice"            => price_info[:take_it_price],
+              "ins0:SecondChanceOfferPrice" => price_info[:second_chance_offer_price],
+              "ins0:StorePrice"             => price_info[:store_price]
+            }
+          end
+
+          client.request :update_inventory_item_quantity_and_price_list do
+            soap.header = soap_header
+            soap.body = soap_body
+          end
+        end # update_inventory_item_quantity_and_price_list
+      end # self
+    end # InventoryService
+  end # Services
+end # ChannelAdvisor
--- spec/lib/channeladvisor/services/inventory_service_spec.rb
+++ spec/lib/channeladvisor/services/inventory_service_spec.rb
@@ -46,7 +46,7 @@ module ChannelAdvisor
             InventoryService.update_inventory_item_quantity_and_price("FAKE001", :quantity_info => quantity_info)
             @last_request.should match_valid_xml_body_for :update_item_quantity
           end
-        end # with only quantity data
+        end # with only quantity info
 
         context "with only price info" do
           use_vcr_cassette "responses/inventory_service/update_inventory_item_quantity_and_price/price_only"
@@ -55,7 +55,7 @@ module ChannelAdvisor
             InventoryService.update_inventory_item_quantity_and_price("FAKE001", :price_info => price_info)
             @last_request.should match_valid_xml_body_for :update_item_price
           end
-        end # with only price data
+        end # with only price info
 
         context "with both quantity and price info" do
           use_vcr_cassette "responses/inventory_service/update_inventory_item_quantity_and_price/quantity_and_price"
@@ -64,8 +64,99 @@ module ChannelAdvisor
             InventoryService.update_inventory_item_quantity_and_price("FAKE001", :quantity_info => quantity_info, :price_info => price_info)
             @last_request.should match_valid_xml_body_for :update_item_quantity_and_price
           end
-        end # with only price data
+        end # with both quantity and price info
       end # .update_inventory_item_quantity_and_price
+
+      describe ".update_inventory_item_quantity_and_price_list" do
+        use_vcr_cassette "responses/inventory_service/update_inventory_item_quantity_and_price_list"
+
+        let(:item1) do
+          {
+            :sku => "FAKE001",
+            :quantity_info => {
+              :update_type  => "Absolute",
+              :total        => 5000
+            },
+            :price_info => {
+              :cost                       => 2.99,
+              :retail_price               => 11.99,
+              :starting_price             => 5.99,
+              :reserve_price              => 7.99,
+              :take_it_price              => 9.99,
+              :second_chance_offer_price  => 8.99,
+              :store_price                => 9.49
+            }
+          }
+        end
+
+        let(:item2) do
+          {
+            :sku => "FAKE002",
+            :quantity_info => {
+              :update_type  => "Absolute",
+              :total        => 7500
+            },
+            :price_info => {
+              :cost                       => 3.99,
+              :retail_price               => 10.99,
+              :starting_price             => 4.99,
+              :reserve_price              => 7.99,
+              :take_it_price              => 8.99,
+              :second_chance_offer_price  => 6.99,
+              :store_price                => 8.49
+            }
+          }
+        end
+
+        before(:each) do
+          @last_request, @last_response = nil
+
+          InventoryService.client.config.hooks.define(:update_inventory_item_quantity_and_price_list, :soap_request) do |callback, request|
+            @last_request = request.http
+            @last_response = callback.call
+          end
+        end
+
+        it "returns a SOAP response" do
+          soap_response = InventoryService.update_inventory_item_quantity_and_price_list([item1])
+          soap_response.should be_a Savon::SOAP::Response
+        end # returns a SOAP response
+
+        context "with one item" do
+          context "with only quantity info" do
+            use_vcr_cassette "responses/inventory_service/update_inventory_item_quantity_and_price_list/with_one_item/quantity_only"
+
+            it "sends a valid SOAP request with only quantity info" do
+              item1.delete(:price_info)
+              InventoryService.update_inventory_item_quantity_and_price_list([item1])
+              @last_request.should match_valid_xml_body_for "update_inventory_item_quantity_and_price_list/with_one_item/quantity_only"
+            end
+          end # with only quantity info
+
+          context "with only price info" do
+            use_vcr_cassette "responses/inventory_service/update_inventory_item_quantity_and_price_list/with_one_item/price_only"
+
+            it "sends a valid SOAP request with only price info" do
+              item1.delete(:quantity_info)
+              InventoryService.update_inventory_item_quantity_and_price_list([item1])
+              @last_request.should match_valid_xml_body_for "update_inventory_item_quantity_and_price_list/with_one_item/price_only"
+            end
+          end # with only price info
+
+          context "with both quantity and price info" do
+            use_vcr_cassette "responses/inventory_service/update_inventory_item_quantity_and_price_list/with_one_item/quantity_and_price"
+
+            it "sends a valid SOAP request with both quantity and price info" do
+              InventoryService.update_inventory_item_quantity_and_price_list([item1])
+              @last_request.should match_valid_xml_body_for "update_inventory_item_quantity_and_price_list/with_one_item/quantity_and_price"
+            end
+          end # with both quantity and price info
+        end # with one item
+
+        context "with two items" do
+
+        end # with two items
+      end
     end # InventoryService
   end # Services
 end # ChannelAdvisor
