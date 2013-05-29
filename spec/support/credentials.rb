cae8c5f7cf15db540dbe9840514591ad2f8d5f64
Tanner Donovan <ttdonovan@gmail.com>
2013-05-28 17:06:03 -0700

    updated the spec/support/credentials.rb -- no longer need the ~/.config/channeladvisor.yml file to run 'rake spec'

--- spec/support/credentials.rb
+++ spec/support/credentials.rb
@@ -1,7 +1,19 @@
 require 'yaml'
 
 def sensitive_data
-  @sensitive_data ||= YAML.load_file("#{ENV["HOME"]}/.config/channeladvisor.yml")
+  unless defined?(@sensitive_data)
+    file_path         = File.join(ENV["HOME"], '.config/channeladvisor.yml')
+    sensitive_data    = YAML.load_file(file_path) if File.exists?(file_path)
+    @sensitive_data   = sensitive_data || {
+      'account_ids'   => %w($$ACCOUNT_ID$$),
+      'developer_key' => '$$DEVELOPER_KEY$$',
+      'password'      => '$$PASSWORD$$',
+      'local_ids'     => %w($$LOCAL_ID$$),
+      'account_names' => %w($$ACCOUNT_NAME$$)
+    }
+  end
+
+  return @sensitive_data
 end
 
 RSpec.configure do |config|
